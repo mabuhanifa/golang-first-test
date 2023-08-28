0152b7a58ea01ebb516d1ccc4263d21104132f6e
mabuhanifa <moh.abuhanifa@gmail.com>
2023-08-28 23:05:28 +0600

    string literal in go

--- main.go
+++ main.go
@@ -7,7 +7,10 @@ var print = fmt.Println
 func main() {
 	fmt.Println("Hello, World!")
 	var people string = `checking string literal`
+	name := "Alice"
+	age := 30
+	message := fmt.Sprintf("My name is %s and I am %d years old.", name, age)
 
-	print(people)
+	print(message, people)
 
 }
